2dea833c7c506880f4a291266dccb867ed3c1224
Quynh Nguyen <quynhnguyen003@gmail.com>
2019-07-05 13:57:46 -0700

    init

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-# SlidingTabView
 
-A description of this package.
+<p align="center">
+<img src="https://i.imgur.com/jQBLzkg.gif" width="890 />
+</p>
+
+**SlidingTabView** is a simple Android-Like tab view that is built using the latest and greatest SwiftUI. Almost everything is customizable!
+
+## Installation
+Please use Swift Package Manager to install **SlidingTabView**
+
+## Usage
+Just instantiate and bind it to your state. That is it!
+```swift
+@State private var selectedTabIndex = 0
+SlidingTabView(selection: $selectedTabIndex,tabs: ["First Tab", "Second Tab"]
+```
+
+## Canvas Preview
+```swift
+struct SlidingTabConsumerView : View {
+    @State private var selectedTabIndex = 0
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            SlidingTabView(selection: self.$selectedTabIndex, tabs: ["First", "Second"])
+            (selectedTabIndex == 0 ? Text("First View") : Text("Second View")).padding()
+            Spacer()
+        }
+            .padding(.top, 50)
+            .animation(.none)
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct SlidingTabView_Previews : PreviewProvider {
+    static var previews: some View {
+        SlidingTabConsumerView()
+    }
+}
+```
+
+## Suggestions or feedback?
+Feel free to create a pull request!
--- Sources/SlidingTabView/SlidingTabView.swift
+++ Sources/SlidingTabView/SlidingTabView.swift
@@ -1,3 +1,168 @@
-struct SlidingTabView {
-    var text = "Hello, World!"
+//
+//  SlidingTabView.swift
+//
+//  Copyright (c) 2019 Quynh Nguyen
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct SlidingTabView : View {
+    
+    // MARK: Internal State
+    
+    /// Internal state to keep track of the selection index
+    @State private var selectionState: Int = 0 {
+        didSet {
+            selection = selectionState
+        }
+    }
+    
+    // MARK: Required Properties
+    
+    /// Binding the selection index which will  re-render the consuming view
+    @Binding var selection: Int
+    
+    /// The title of the tabs
+    let tabs: [String]
+    
+    // Mark: View Customization Properties
+    
+    /// The font of the tab title
+    var font: Font = .body
+    
+    /// The selection bar sliding animation type
+    var animation: Animation = .spring(damping: 12)
+    
+    /// The accent color when the tab is selected
+    var activeAccentColor: Color = Color.blue
+    
+    /// The accent color when the tab is not selected
+    var inactiveAccentColor: Color = Color.black.opacity(0.4)
+    
+    /// The color of the selection bar
+    var selectionBarColor: Color = Color.blue
+    
+    /// The tab color when the tab is not selected
+    var inactiveTabColor: Color = Color.clear
+    
+    /// The tab color when the tab is  selected
+    var activeTabColor: Color = Color.clear
+    
+    /// The height of the selection bar
+    var selectionBarHeight: Length = 2
+    
+    /// The selection bar background color
+    var selectionBarBackgroundColor = Color.gray.opacity(0.2)
+    
+    /// The height of the selection bar background
+    var selectionBarBackgroundHeight: Length = 1
+    
+    
+    
+    // MARK: View Construction
+    
+    var body: some View {
+        assert(tabs.count > 1, "Must have at least 2 tabs")
+        
+        return VStack(alignment: .leading, spacing: 0) {
+            HStack(spacing: 0) {
+                ForEach(self.tabs.identified(by: \.self)) { tab in
+                    Button(action: {
+                        let selection = self.tabs.firstIndex(of: tab) ?? 0
+                        self.selectionState = selection
+                    }) {
+                        HStack {
+                            Spacer()
+                            Text(tab).font(self.font)
+                            Spacer()
+                        }
+                    }
+                    .padding(.vertical, 16)
+                        .accentColor(
+                            self.isSelected(tabIdentifier: tab)
+                                ? self.activeAccentColor
+                                : self.inactiveAccentColor)
+                        .background(
+                            self.isSelected(tabIdentifier: tab)
+                                ? self.activeTabColor
+                                : self.inactiveTabColor)
+                }
+            }
+            GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .fill(self.selectionBarColor)
+                        .frame(width: self.tabWidth(from: geometry.size.width), height: self.selectionBarHeight, alignment: .leading)
+                        .offset(x: self.selectionBarXOffset(from: geometry.size.width), y: 0)
+                        .animation(self.animation)
+                    Rectangle()
+                        .fill(self.selectionBarBackgroundColor)
+                        .frame(width: geometry.size.width, height: self.selectionBarBackgroundHeight, alignment: .leading)
+                }.fixedSize(horizontal: false, vertical: true)
+            }.fixedSize(horizontal: false, vertical: true)
+            
+        }
+    }
+    
+    // MARK: Private Helper
+    
+    private func isSelected(tabIdentifier: String) -> Bool {
+        return tabs[selectionState] == tabIdentifier
+    }
+    
+    private func selectionBarXOffset(from totalWidth: CGFloat) -> CGFloat {
+        return self.tabWidth(from: totalWidth) * CGFloat(selectionState)
+    }
+    
+    private func tabWidth(from totalWidth: CGFloat) -> CGFloat {
+        return totalWidth / CGFloat(tabs.count)
+    }
 }
+
+#if DEBUG
+
+@available(iOS 13.0, *)
+struct SlidingTabConsumerView : View {
+    @State private var selectedTabIndex = 0
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            SlidingTabView(selection: self.$selectedTabIndex,
+                           tabs: ["First", "Second"],
+                           font: .body,
+                           activeAccentColor: Color.blue,
+                           selectionBarColor: Color.blue)
+            (selectedTabIndex == 0 ? Text("First View") : Text("Second View")).padding()
+            Spacer()
+        }
+        .padding(.top, 50)
+            .animation(.none)
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct SlidingTabView_Previews : PreviewProvider {
+    static var previews: some View {
+        SlidingTabConsumerView()
+    }
+}
+#endif
