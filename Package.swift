c2002f7414a1d3fb0809065ecf90c5606849206e
Quynh Nguyen <quynhnguyen003@gmail.com>
2019-07-05 14:32:40 -0700

    add path to package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SlidingTabView",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "SlidingTabViewTests",
             dependencies: ["SlidingTabView"]),
