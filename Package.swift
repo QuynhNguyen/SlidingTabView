55d4d5da7cdecd964951ad4b78ca80c0606e86d7
Marco A. Solis Cruz <marco@msolis.com.mx>
2020-03-31 12:04:40 -0600

    Update Package.swift
    fix: Data need for archive

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SlidingTabView",
+    platforms: [
+      .iOS(.v13), .watchOS(.v6), .tvOS(.v13), .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
